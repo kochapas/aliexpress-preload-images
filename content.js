1cf972b4a439de7a96318a3582f9347948e7458c
Kochapas Santibut <kochapas@outlook.com>
2023-04-18 22:08:04 +0900

    Add logic to select the initial variants after the images are loaded.

--- content.js
+++ content.js
@@ -1,8 +1,14 @@
 // Select all variant elements.
 const items = document.querySelectorAll(".sku-property-item");
-console.log(items);
 // Loop foreach variant image elements.
 items?.forEach((item) => {
   // Click each item to force the image to be loaded.
-  item.click();
+  item?.click();
+});
+
+// After everything is clicked, click the first item in each variant section again to reset the selected state.
+const lists = document.querySelectorAll(".sku-property-list");
+lists?.forEach((list) => {
+  // This will also distract the user to look at the early variants first while the later one are loading their images.
+  if (list?.children?.length) list.children[0].click();
 });
