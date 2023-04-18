b823b389bdfab1123e602332d435af293344fc92
Kochapas Santibut <kochapas@outlook.com>
2023-04-18 21:47:59 +0900

    Git init with codes cloned from previous project.

---
+++ README.md
@@ -0,0 +1,2 @@
+# aliexpress-preload-images
+A chrome extensions to preload variants' images on Aliexpress.
---
+++ content.js
@@ -0,0 +1,10 @@
+// Select all variant elements.
+
+// Loop foreach variant image elements.
+  // Click each item to force the image to be loaded.
+
+  // If this is the last item in the list, click on the first one to go back to the initial state.
+
+  // This will also distract the user to look at the early variants first while the later one are loading their images.
+
+// End the operation. This is pretty much it.
