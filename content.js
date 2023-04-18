a62cd6b189924655977828d7c08209e9f0c42fff
Kochapas Santibut <kochapas@outlook.com>
2023-04-18 22:04:49 +0900

    Add logic to click every variants when the page is loaded to force loading the images.

--- content.js
+++ content.js
@@ -1,10 +1,8 @@
 // Select all variant elements.
-
+const items = document.querySelectorAll(".sku-property-item");
+console.log(items);
 // Loop foreach variant image elements.
+items?.forEach((item) => {
   // Click each item to force the image to be loaded.
-
-  // If this is the last item in the list, click on the first one to go back to the initial state.
-
-  // This will also distract the user to look at the early variants first while the later one are loading their images.
-
-// End the operation. This is pretty much it.
+  item.click();
+});
